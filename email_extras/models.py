f28f85277568f8c762b9a00eaad2af25cc2ddec8
Jannik Schürg <jannik.schuerg@posteo.de>
2018-03-02 13:23:30 +0100

    Implement a clean method for Key model

--- email_extras/models.py
+++ email_extras/models.py
@@ -1,5 +1,11 @@
 
 from __future__ import unicode_literals
+
+import re
+from time import time
+
+from django.core.exceptions import ValidationError
+from django.contrib import messages
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
@@ -10,6 +16,11 @@ from email_extras.utils import addresses_for_key
 
 if USE_GNUPG:
     from gnupg import GPG
+
+    RE_CLEAN_KEY = re.compile(
+        "(" + 
+            "|".join([re.escape(c) for c in r'''~!@#$%^&*()_+`-={}|[]\;':"<>?,./- ''']) +
+        ")*(?=-----(?:BEGIN|END) PGP)")
 
     @python_2_unicode_compatible
     class Key(models.Model):
@@ -34,6 +45,42 @@ if USE_GNUPG:
         @property
         def email_addresses(self):
             return ",".join(str(address) for address in self.address_set.all())
+
+        def clean(self, request=None):
+            """
+            Validates the key.
+
+            If request is given warnings may be added using Django's messages
+            framework.
+            """
+            super().clean()
+            # PGP does accept some additional characters which we need to
+            # remove for GPG to accept the key
+            self.key = re.sub(RE_CLEAN_KEY, '', self.key.strip())
+            gpg = GPG()
+            result = gpg.import_keys(self.key)
+
+            if result.count == 0:
+                raise ValidationError(_("No key was found"))
+            if result.count > 1:
+                raise ValidationError(_("More than one key was imported"))
+            if result.n_revoc > 0:
+                raise ValidationError(_("The key is revoked"))
+
+            assert len(result.fingerprints) == 1
+            fp = result.fingerprints[0]
+            key_data = next(k for k in gpg.list_keys() if k["fingerprint"] == fp)
+            if 'expires' in key_data:
+                if int(key_data['expires']) < time():
+                    raise ValidationError(_("The key is expired"))
+
+            problems = [(result.problem_reason[key['problem']], key.get('text'))
+                        for key in result.results if 'problem' in key]
+            if problems and request:
+                problem_text = _("There problems with the PGP key: ") + \
+                    ".".join(_(text or reason) for reason, text in problems) + \
+                    "."
+                messages.warning(request, problem_text)
 
         def save(self, *args, **kwargs):
             gpg = GPG(gnupghome=GNUPG_HOME)
