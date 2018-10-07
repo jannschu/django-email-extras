25c303b25acdba1069bd633d56434a2ec544b360
Jannik Schürg <jannik.schuerg@posteo.de>
2018-10-06 22:13:26 -0700

    Remove »USE_GNUPG« setting

--- email_extras/admin.py
+++ email_extras/admin.py
@@ -1,23 +1,19 @@
-from email_extras.settings import USE_GNUPG
+from django.contrib import admin
 
+from email_extras.models import Key, Address
+from email_extras.forms import KeyForm
 
-if USE_GNUPG:
-    from django.contrib import admin
+class KeyAdmin(admin.ModelAdmin):
+    form = KeyForm
+    list_display = ('__str__', 'email_addresses')
+    readonly_fields = ('fingerprint', )
 
-    from email_extras.models import Key, Address
-    from email_extras.forms import KeyForm
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'key')
+    readonly_fields = ('key', )
 
-    class KeyAdmin(admin.ModelAdmin):
-        form = KeyForm
-        list_display = ('__str__', 'email_addresses')
-        readonly_fields = ('fingerprint', )
+    def has_add_permission(self, request):
+        return False
 
-    class AddressAdmin(admin.ModelAdmin):
-        list_display = ('__str__', 'key')
-        readonly_fields = ('key', )
-
-        def has_add_permission(self, request):
-            return False
-
-    admin.site.register(Key, KeyAdmin)
-    admin.site.register(Address, AddressAdmin)
+admin.site.register(Key, KeyAdmin)
+admin.site.register(Address, AddressAdmin)
--- email_extras/settings.py
+++ email_extras/settings.py
@@ -1,13 +1,5 @@
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 
 GNUPG_HOME = getattr(settings, "EMAIL_EXTRAS_GNUPG_HOME", None)
-USE_GNUPG = getattr(settings, "EMAIL_EXTRAS_USE_GNUPG", GNUPG_HOME is not None)
 ALWAYS_TRUST = getattr(settings, "EMAIL_EXTRAS_ALWAYS_TRUST_KEYS", False)
 SIGN = getattr(settings, "EMAIL_EXTRAS_SIGN", False)
-
-if USE_GNUPG:
-    try:
-        from pretty_bad_protocol import gnupg  # noqa: F401
-    except ImportError:
-        raise ImproperlyConfigured("Could not import gnupg")
